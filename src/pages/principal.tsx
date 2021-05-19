55b43e4eefc5d16c91a70fba5a5f968fcf9ff72f
Fernando Dutra <fernandozek@hotmail.com>
2021-05-19 01:46:44 -0300

    40% pagina principal

--- src/pages/principal.tsx
+++ src/pages/principal.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import NavBar from '../components/Header/NavBar';
 import Footer from '../components/Footer/Container';
+import ImgProduto from '../images/produto1.png';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import { BiShoppingBag } from 'react-icons/bi';
 import {
-    Container
+    ButtonNext,
+    ButtonPrev,
+    CategoriaItems,
+    CategoriaLista,
+    CategoriaOptions,
+    Categorias,
+    Container,
+    Data,
+    ExibeProduto,
+    ImgProdutoItem,
+    InfoProduto,
+    Item,
+    MaisComprados,
+    Novidades,
+    PrincipalTop,
+    ProdutoButton,
+    ProdutoDe,
+    ProdutoFavorito,
+    ProdutoNome,
+    ProdutoOu,
+    ProdutoPor,
+    Produtos,
+    ProdutosTop,
+    Title
 } from '../styles/pages/principal';
 const Principal: React.FC = () => {
+
+    const [favoritoState, setFavoritoState] = useState(false);
+
+    function setFavorito() {
+        if (favoritoState) {
+            setFavoritoState(false);
+        } else {
+            setFavoritoState(true);
+        }
+    }
 
     return (
         <Container>
@@ -13,6 +49,350 @@ const Principal: React.FC = () => {
                 <title>Tela Inicial</title>
             </Head>
             <NavBar />
+            <PrincipalTop>
+                <Categorias>
+                    <CategoriaOptions>
+                        <CategoriaLista>
+                            <CategoriaItems>Todos os produtos</CategoriaItems>
+                            <CategoriaItems>Beleza e Saúde</CategoriaItems>
+                            <CategoriaItems>Casa, móveis e decoração</CategoriaItems>
+                            <CategoriaItems>Eletrodomésticos</CategoriaItems>
+                            <CategoriaItems>Eletrônicos</CategoriaItems>
+                            <CategoriaItems>Esporte e lazer </CategoriaItems>
+                            <CategoriaItems>Pet Shop</CategoriaItems>
+                            <CategoriaItems>Outros</CategoriaItems>
+                        </CategoriaLista>
+                    </CategoriaOptions>
+                </Categorias>
+                <ProdutosTop>
+                    <ButtonPrev></ButtonPrev>
+                    <Produtos>
+                        <Item>
+                            <ExibeProduto>
+                                <InfoProduto>
+                                    <ImgProdutoItem src={ImgProduto} />
+                                    <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                    <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                    <ProdutoPor>R$1.300,00</ProdutoPor>
+                                    <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                                </InfoProduto>
+                                <ProdutoFavorito onClick={setFavorito}>
+                                    {
+                                        favoritoState &&
+                                        <AiFillHeart />
+                                    }
+                                    {
+                                        !favoritoState &&
+                                        <AiOutlineHeart />
+                                    }
+                                </ProdutoFavorito>
+                            </ExibeProduto>
+                            <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                        </Item>
+                        <Item>
+                            <ExibeProduto>
+                                <InfoProduto>
+                                    <ImgProdutoItem src={ImgProduto} />
+                                    <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                    <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                    <ProdutoPor>R$1.300,00</ProdutoPor>
+                                    <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                                </InfoProduto>
+                                <ProdutoFavorito onClick={setFavorito}>
+                                    {
+                                        favoritoState &&
+                                        <AiFillHeart />
+                                    }
+                                    {
+                                        !favoritoState &&
+                                        <AiOutlineHeart />
+                                    }
+                                </ProdutoFavorito>
+                            </ExibeProduto>
+                            <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                        </Item>
+                        <Item>
+                            <ExibeProduto>
+                                <InfoProduto>
+                                    <ImgProdutoItem src={ImgProduto} />
+                                    <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                    <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                    <ProdutoPor>R$1.300,00</ProdutoPor>
+                                    <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                                </InfoProduto>
+                                <ProdutoFavorito onClick={setFavorito}>
+                                    {
+                                        favoritoState &&
+                                        <AiFillHeart />
+                                    }
+                                    {
+                                        !favoritoState &&
+                                        <AiOutlineHeart />
+                                    }
+                                </ProdutoFavorito>
+                            </ExibeProduto>
+                            <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                        </Item>
+                        <Item>
+                            <ExibeProduto>
+                                <InfoProduto>
+                                    <ImgProdutoItem src={ImgProduto} />
+                                    <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                    <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                    <ProdutoPor>R$1.300,00</ProdutoPor>
+                                    <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                                </InfoProduto>
+                                <ProdutoFavorito onClick={setFavorito}>
+                                    {
+                                        favoritoState &&
+                                        <AiFillHeart />
+                                    }
+                                    {
+                                        !favoritoState &&
+                                        <AiOutlineHeart />
+                                    }
+                                </ProdutoFavorito>
+                            </ExibeProduto>
+                            <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                        </Item>
+                    </Produtos>
+                    <ButtonNext></ButtonNext>
+                </ProdutosTop>
+            </PrincipalTop>
+            <Novidades>
+                <Title>Últimas novidades <Data>07/05/2021</Data> </Title>
+                <Produtos>
+                    <ButtonPrev></ButtonPrev>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <ButtonNext></ButtonNext>
+                </Produtos>
+            </Novidades>
+            <MaisComprados>
+                <Title>Mais Comprados</Title>
+                <Produtos>
+                    <ButtonPrev></ButtonPrev>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <Item>
+                        <ExibeProduto>
+                            <InfoProduto>
+                                <ImgProdutoItem src={ImgProduto} />
+                                <ProdutoNome>Smartphone Xiaomi Redmi Note 9s</ProdutoNome>
+                                <ProdutoDe>de R$1.700,00 por</ProdutoDe>
+                                <ProdutoPor>R$1.300,00</ProdutoPor>
+                                <ProdutoOu>ou 10x de 130,00</ProdutoOu>
+                            </InfoProduto>
+                            <ProdutoFavorito onClick={setFavorito}>
+                                {
+                                    favoritoState &&
+                                    <AiFillHeart />
+                                }
+                                {
+                                    !favoritoState &&
+                                    <AiOutlineHeart />
+                                }
+                            </ProdutoFavorito>
+                        </ExibeProduto>
+                        <ProdutoButton>Adicionar no carrinho <BiShoppingBag /> </ProdutoButton>
+                    </Item>
+                    <ButtonNext></ButtonNext>
+                </Produtos>
+            </MaisComprados>
             <Footer />
         </Container>
     );
--- src/styles/pages/principal.ts
+++ src/styles/pages/principal.ts
@@ -20,3 +20,184 @@ export const Menu = styled.div`
         width: 200px;
     }
 `
+export const PrincipalTop = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`
+export const Categorias = styled.section`
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 10px;
+`
+export const CategoriaOptions = styled.div`
+    width: 240px;
+    height: 270px;
+    border: 1px solid var(--c4);
+    border-radius: 13px;
+    padding: 10px;
+    font-size: 90%;
+`
+export const CategoriaItems = styled.li`
+    margin-bottom: 10px;
+    list-style: none;
+    cursor: pointer;
+`
+export const CategoriaLista = styled.ul`
+    margin-left: 20px;
+    color: #4D4D4D;
+    ${CategoriaItems}:first-child{
+        color: #233B6C;
+        font-weight: bold;
+    }
+`
+export const Title = styled.h2`
+    width: 200px;
+    margin-right: auto;
+    margin-left: 6%;
+    margin-bottom: 15px;
+    font-size: 90%;
+    color: #233B6C;
+    display: flex;
+    align-items: center;
+    text-align: start;
+`
+export const Data = styled.p`
+    margin-left: 5px;
+    margin-top: 3px;
+    font-size: 70%;
+    color: #8A8A8A;
+`
+export const ProdutosTop = styled.div`
+    width: 75%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const Produtos = styled.section`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+export const Novidades = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+export const MaisComprados = styled.section`
+    width: 100%;
+
+`
+export const BelezaESaude = styled.section`
+    width: 100%;
+
+`
+export const CategoriaEmDestaque = styled.section`
+    width: 100%;
+
+`
+export const MaisAcessados = styled.section`
+    width: 100%;
+
+`
+export const ProdutoFavorito = styled.div`
+    color: #4D4D4D;
+    height: 20px;
+    opacity: 0;
+    svg{
+        font-size: 130%;
+    }
+    cursor: pointer;
+`
+export const Item = styled.div`
+    width: 200px;
+    height: 270px;
+    border: 1px solid var(--c4);
+    border-radius: 13px;
+    margin: 0 15px;
+    padding: 20px 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    :hover{
+        ${ProdutoFavorito}{
+            opacity: 1;
+        }
+    }
+`
+export const ButtonPrev = styled.button`
+    width: 25px;
+    height: 35px;
+    background-color: #1976D2;
+    opacity: 0.2;
+    margin-left: 20px;
+`
+export const ButtonNext = styled.button`
+    width: 25px;
+    height: 35px;
+    background-color: #1976D2;
+    opacity: 0.2;
+    margin-right: 20px;
+`
+export const ExibeProduto = styled.div`
+    display:flex;
+`
+export const InfoProduto = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: 90%;
+`
+export const ImgProdutoItem = styled.img`
+    width: 70px;
+    height: 70px;
+    margin: 0 auto;
+`
+export const ProdutoNome = styled.p`
+    width: 140px;
+    font-weight: bold;
+    color: #233B6C;
+    margin-bottom: 10px;
+`
+export const ProdutoDe = styled.p`
+    color: #BABABA;
+    font-size: 80%;
+`
+export const ProdutoPor = styled.p`
+    font-weight: bold;
+    font-size: 120%;
+    color: #233B6C;
+`
+export const ProdutoOu = styled.p`
+    color: #4D4D4D;
+    font-size: 90%;
+`
+
+export const ProdutoButton = styled.button`
+    width: 90%;
+    height: 30px;
+    font-size: 70%;
+    border-radius: 5px;
+    background: rgb(58,83,135);
+    background: linear-gradient(90deg, rgba(58,83,135,1) 0%, rgba(54,111,227,1) 100%);
+    color: #FFFFFF;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 0.6;
+    svg{
+        margin-left: 4px;
+        font-size: 110%;
+    }
+    :hover{
+        opacity: 1;
+
+    }
+`
